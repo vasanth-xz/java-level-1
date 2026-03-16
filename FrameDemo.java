0b6a437593713c853c89a286bfd549bc524ca0fc
vasanth-xz <vasanthsaravanan357@gmail.com>
2026-03-16 12:45:03 +0530

    framedemo.java

---
+++ FrameDemo.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+class FrameDemo
+{
+    public static void main(String args[])
+    {
+        Frame s = new Frame();
+
+        s.setTitle("vasanth - BCA");
+        s.setSize(500, 400);
+        s.setVisible(true);
+    }
+}
